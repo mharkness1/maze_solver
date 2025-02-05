b77134fab8b5538eb3741986c5a11e4bbb3a49b3
mharkness1 <mharkness.uk@gmail.com>
2025-02-05 21:06:53 +0000

    Alterations to file structure. Start of graphics logic.

--- src/graphics.py
+++ src/graphics.py
@@ -1,6 +1,6 @@
 from tkinter import Tk, BOTH, Canvas
 
-class WindowClass:
+class Window:
     def __init__(self, width, height):
         self.__root = Tk()
         self.__root.title("Maze Solver")
@@ -20,4 +20,17 @@ class WindowClass:
 
     def close(self):
         self.__running = False
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+
+    def draw(self):
+        pass
 
--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 from tkinter import Tk, BOTH, Canvas
-from WindowClass import WindowClass
+from graphics import Window
 
 def main():
-    win = WindowClass(800, 600)
+    win = Window(800, 600)
     win.wait_for_close()
 
 if __name__ == "__main__":
