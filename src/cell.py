675724cf53e9c03fdd013d96401b8e00829cf93e
mharkness1 <mharkness.uk@gmail.com>
2025-02-05 21:44:17 +0000

    Cell implementation finished. Needs robust testing.

--- src/cell.py
+++ src/cell.py
@@ -1,3 +1,5 @@
+from graphics import Line, Point
+
 class Cell:
     def __init__(self, win):
         self.left_wall = True
@@ -15,4 +17,16 @@ class Cell:
         self._x2 = x2
         self._y1 = y1
         self._y2 = y2
-        pass
+        
+        if self.left_wall:
+            line = Line(Point(x1,y1), Point(x1,y2))
+            self._win.draw_line(line)
+        if self.top_wall:
+            line = Line(Point(x1,y1), Point(x2,y1))
+            self._win.draw_line(line)
+        if self.right_wall:
+            line = Line(Point(x2,y1), Point(x2,y2))
+            self._win.draw_line(line)
+        if self.bottom_wall:
+            line = Line(Point(x1,y2), Point(x2,y2))
+            self._win.draw_line(line)
--- src/main.py
+++ src/main.py
@@ -3,8 +3,10 @@ from graphics import Window, Line, Point
 
 def main():
     win = Window(800, 600)
+    #TO DO Generate some cells to test the implementation.
     line = Line(Point(50,50), Point(400,400))
     win.draw_line(line, "white")
+    
     win.wait_for_close()
 
 if __name__ == "__main__":
